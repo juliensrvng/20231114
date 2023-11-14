fa2de5ab80d6b1b19862f3436e40f1f674bbf852
Julien Sourivong <julien.sourivong@gmail.com>
2023-11-14 15:36:08 +0100

    modif sidebar

--- admin/includes/sidebar.php
+++ admin/includes/sidebar.php
@@ -37,8 +37,8 @@
     <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionSidebar">
         <div class="bg-white py-2 collapse-inner rounded">
             <h6 class="collapse-header">Custom Components:</h6>
-            <a class="collapse-item" href="buttons.php">Buttons</a>
-            <a class="collapse-item" href="cards.php">Cards</a>
+            <a class="collapse-item" href="../views/buttons.php">Buttons</a>
+            <a class="collapse-item" href="../views/cards.php">Cards</a>
         </div>
     </div>
 </li>
@@ -54,10 +54,10 @@
         data-parent="#accordionSidebar">
         <div class="bg-white py-2 collapse-inner rounded">
             <h6 class="collapse-header">Custom Utilities:</h6>
-            <a class="collapse-item" href="utilities-color.php">Colors</a>
-            <a class="collapse-item" href="utilities-border.php">Borders</a>
-            <a class="collapse-item" href="utilities-animation.php">Animations</a>
-            <a class="collapse-item" href="utilities-other.php">Other</a>
+            <a class="collapse-item" href="../views/utilities-color.php">Colors</a>
+            <a class="collapse-item" href="../views/utilities-border.php">Borders</a>
+            <a class="collapse-item" href="../views/utilities-animation.php">Animations</a>
+            <a class="collapse-item" href="../views/utilities-other.php">Other</a>
         </div>
     </div>
 </li>
@@ -71,37 +71,37 @@
 </div>
 
 <!-- Nav Item - Pages Collapse Menu -->
-<li class="nav-item active">
+<li class="nav-item">
     <a class="nav-link" href="#" data-toggle="collapse" data-target="#collapsePages" aria-expanded="true"
         aria-controls="collapsePages">
         <i class="fas fa-fw fa-folder"></i>
         <span>Pages</span>
     </a>
-    <div id="collapsePages" class="collapse show" aria-labelledby="headingPages"
+    <div id="collapsePages" class="collapse" aria-labelledby="headingPages"
         data-parent="#accordionSidebar">
         <div class="bg-white py-2 collapse-inner rounded">
             <h6 class="collapse-header">Login Screens:</h6>
-            <a class="collapse-item" href="login.php">Login</a>
-            <a class="collapse-item" href="register.php">Register</a>
-            <a class="collapse-item" href="forgot-password.php">Forgot Password</a>
+            <a class="collapse-item" href="../views/login.php">Login</a>
+            <a class="collapse-item" href="../views/register.php">Register</a>
+            <a class="collapse-item" href="../views/forgot-password.php">Forgot Password</a>
             <div class="collapse-divider"></div>
             <h6 class="collapse-header">Other Pages:</h6>
-            <a class="collapse-item active" href="404.php">404 Page</a>
-            <a class="collapse-item" href="blank.php">Blank Page</a>
+            <a class="collapse-item" href="../views/404.php">404 Page</a>
+            <a class="collapse-item" href="../views/blank.php">Blank Page</a>
         </div>
     </div>
 </li>
 
 <!-- Nav Item - Charts -->
 <li class="nav-item">
-    <a class="nav-link" href="charts.php">
+    <a class="nav-link" href="../views/charts.php">
         <i class="fas fa-fw fa-chart-area"></i>
         <span>Charts</span></a>
 </li>
 
 <!-- Nav Item - Tables -->
 <li class="nav-item">
-    <a class="nav-link" href="tables.php">
+    <a class="nav-link" href="../views/tables.php">
         <i class="fas fa-fw fa-table"></i>
         <span>Tables</span></a>
 </li>
